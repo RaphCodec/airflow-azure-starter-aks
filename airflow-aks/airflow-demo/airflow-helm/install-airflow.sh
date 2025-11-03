f59cf92e5ea1e34ee627b8305bd6d1a3c090d62c
RaphCodeC <46251144+RaphCodec@users.noreply.github.com>
2025-11-03 04:41:19 +0000

    feat(airflow-aks-demo): add anisble and bash scripts to provision Azure infastructure and deploy basic Airflow 3.1.1 on AKS with helm and cusotm helm chart

--- airflow-aks/airflow-demo/airflow-helm/install-airflow.sh
+++ airflow-aks/airflow-demo/airflow-helm/install-airflow.sh
@@ -8,16 +8,44 @@ export AKS_AIRFLOW_CLUSTER_NAME
 export AKS_AIRFLOW_RESOURCE_GROUP
 export AKS_AIRFLOW_NAMESPACE
 export AKS_AIRFLOW_HELM_RELEASE_NAME
+export TENANT_ID
+export MANAGED_IDENTITY_CLIENT_ID
+export SERVICE_ACCOUNT_NAME
+
+echo "Starting Airflow installation on AKS..."
 
 # Get AKS credentials
-az aks get-credentials --resource-group $AKS_AIRFLOW_RESOURCE_GROUP --name $AKS_AIRFLOW_CLUSTER_NAME
+az aks get-credentials --resource-group $AKS_AIRFLOW_RESOURCE_GROUP --name $AKS_AIRFLOW_CLUSTER_NAME --overwrite-existing
+echo "Updated kubeconfig with AKS cluster credentials."
+
+echo "Creating Airflow Service Account with Workload Identity..."
+
+# CREATE AIRFLOW SERVICE ACCOUNT
+export TENANT_ID=$(az account show --query tenantId -o tsv)
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    azure.workload.identity/client-id: "${MANAGED_IDENTITY_CLIENT_ID}"
+    azure.workload.identity/tenant-id: "${TENANT_ID}"
+  name: "${SERVICE_ACCOUNT_NAME}"
+  namespace: "${AKS_AIRFLOW_NAMESPACE}"
+EOF
+
+# Attach ACR to AKS cluster.
+# This step is necessary to allow the AKS cluster to pull images from the ACR.
+# This step can be commented out if already done as it takes the most time.
+echo "Attaching ACR '${ACR_REGISTRY}' to AKS cluster..."
+az aks update --resource-group $AKS_AIRFLOW_RESOURCE_GROUP --name $AKS_AIRFLOW_CLUSTER_NAME --attach-acr $ACR_REGISTRY
+echo "ACR successfully attached to AKS cluster."
 
 # Check if Airflow release exists and uninstall if it does
 if helm list -n $AKS_AIRFLOW_NAMESPACE | grep -q $AKS_AIRFLOW_HELM_RELEASE_NAME; then
     echo "Existing Airflow release found. Uninstalling..."
     helm uninstall $AKS_AIRFLOW_HELM_RELEASE_NAME -n $AKS_AIRFLOW_NAMESPACE
     echo "Waiting for pods to terminate..."
-    kubectl wait --for=delete pods --all -n $AKS_AIRFLOW_NAMESPACE --timeout=300s
+    kubectl wait --for=delete pods --all -n $AKS_AIRFLOW_NAMESPACE --timeout=300s || true
 else
     echo "No existing Airflow release found. Proceeding with fresh install..."
 fi
