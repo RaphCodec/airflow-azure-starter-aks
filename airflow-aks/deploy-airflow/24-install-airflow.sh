593e9f041609d4c4ef5d6514489fd47556e46089
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-11-16 21:58:31 -0500

    feat(airflow): add github dag bundles configuration

--- airflow-aks/deploy-airflow/24-install-airflow.sh
+++ airflow-aks/deploy-airflow/24-install-airflow.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Source environment variables
+source 01-set-env-vars.sh
+
 helm repo add apache-airflow https://airflow.apache.org
 helm repo update
 helm search repo airflow
@@ -5,6 +9,7 @@ helm search repo airflow
 # Process the values file to substitute environment variables
 echo "Processing airflow-values.yaml with environment variables..."
 envsubst < airflow-values.yaml > airflow-values-processed.yaml
+
 
 helm upgrade --install airflow apache-airflow/airflow --namespace airflow-demo --create-namespace -f airflow-values-processed.yaml --debug
 
